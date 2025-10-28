c6733834a1adb62583352e37cf2903c44e3555f1
Jonathan Warner <jono8367@gmail.com>
2025-10-28 15:33:28 -0400

    Finished dark mode implementation across all elements, animation implementation via motion started

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -2,7 +2,7 @@ import { assets, workData } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
 
-const Work = () => {
+const Work = ({isDarkMode}) => {
   return (
     <div id='work' className='w-full px-[12%] py-10 scroll-mt-20'>
         <h4 className='text-center mb-2 text-lg font-Ovo'>
@@ -13,7 +13,7 @@ const Work = () => {
             Welcome to my software development portfolio! Explore a collection of my previous projects that showcase my expertise in software development.
         </p>
 
-    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 my-10'>
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 my-10 dark:text-black'>
         {workData.map((project, index)=>(
             <div key={index}
             className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursour-pointer group'
@@ -31,8 +31,9 @@ const Work = () => {
         ))}
     </div>
 
-    <a href="" className='w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-lightHover duration-500'>
-        Show more <Image src={assets.right_arrow_bold} alt='Right arrow' className='w-4' />
+    <a href="" className='w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-lightHover duration-500 
+    dark:text-white dark:border-white dark:hover:bg-darkHover'>
+        Show more <Image src={isDarkMode ? assets.right_arrow_bold_dark : assets.right_arrow_bold} alt='Right arrow' className='w-4' />
     </a>
     </div>
     
